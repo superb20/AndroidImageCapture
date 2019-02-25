cbabd34c39b83af81599dbe87c66b99cf093fdbe
superb20 <superb20.lee@gmail.com>
2019-02-26 01:44:15 +0900

    add upload module

--- ImageCpature/app/src/main/java/com/superb20/imagecapture/MainFragment.java
+++ ImageCpature/app/src/main/java/com/superb20/imagecapture/MainFragment.java
@@ -2,18 +2,17 @@ package com.superb20.imagecapture;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.superb20.imagecapture.Common.PermissionHelper;
@@ -23,6 +22,14 @@ import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 /**
  * Created by superb20 on 2019-02-25.
  */
@@ -30,6 +37,7 @@ import java.util.Date;
 public class MainFragment extends Fragment implements View.OnClickListener {
     private final static String TAG = "MainFragment";
     private static final int REQUEST_PHOTO = 0;
+    private static final int REQUEST_GALLERY = 1;
 
     private File mPhotoFile = null;
 
@@ -37,7 +45,7 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     public void onResume() {
         super.onResume();
 
-        if(!PermissionHelper.hasStoragePermission(getActivity())) {
+        if (!PermissionHelper.hasStoragePermission(getActivity())) {
             Log.d(TAG, "has not storage permission");
             PermissionHelper.requestStoragePermission(getActivity());
             return;
@@ -50,15 +58,20 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, parent, false);
         v.findViewById(R.id.btn_capture).setOnClickListener(this);
+        v.findViewById(R.id.btn_upload).setOnClickListener(this);
 
         return v;
     }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.btn_capture:
                 imageCapture();
+                break;
+
+            case R.id.btn_upload:
+                loadAlbum();
                 break;
         }
     }
@@ -70,8 +83,12 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
         if (requestCode == REQUEST_PHOTO) {
             notifyMediaStoreScanner(mPhotoFile);
+            Toast.makeText(getContext(), "Image captured", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == REQUEST_GALLERY) {
+            imageUpload(data);
         }
     }
+
 
     private void imageCapture() {
         Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -94,6 +111,31 @@ public class MainFragment extends Fragment implements View.OnClickListener {
             Log.e(TAG, "notifyMediaStoreScanner fail : " + e.toString());
             e.printStackTrace();
         }
+    }
+
+    private void loadAlbum() {
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    private void imageUpload(Intent data) {
+        Uri selectedImage = data.getData();
+        String path = getPath(selectedImage);
+        Toast.makeText(getContext(), "picked path : " + path, Toast.LENGTH_SHORT).show();
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.Video.Media.DATA};
+        Cursor cursor = getContext().getContentResolver().query(uri, projection, null, null, null);
+
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
     }
 
     private String getPhotoFileName() {
